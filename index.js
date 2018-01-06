51ad06358b1e7cb4f6441f9efea07443a2dff2d1
Ward van Teijlingen <w.van.teijlingen@gmail.com>
2018-01-06 12:36:21 +0100

    Added Activities constants.

--- index.js
+++ index.js
@@ -2,11 +2,13 @@
 
 let { AppleHealthKit } = require('react-native').NativeModules;
 
+import { Activities } from './Constants/Activities'
 import { Permissions } from './Constants/Permissions'
 import { Units } from './Constants/Units'
 
 let HealthKit = Object.assign({}, AppleHealthKit, {
 	Constants: {
+		Activities: Activities,
 		Permissions: Permissions,
 		Units: Units,
 	}
