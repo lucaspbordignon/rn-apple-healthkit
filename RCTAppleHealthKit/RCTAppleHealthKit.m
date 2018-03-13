3669b3aa104d39c713ef8261954541902c226d4f
mhung <m.hung@sproutatwork.com>
2018-03-13 13:23:25 -0400

    Adding two new functions to submit data to internal endpoint and clearing the token in the background

--- RCTAppleHealthKit/RCTAppleHealthKit.m
+++ RCTAppleHealthKit/RCTAppleHealthKit.m
@@ -37,6 +37,16 @@ RCT_EXPORT_METHOD(initHealthKit:(NSDictionary *)input callback:(RCTResponseSende
     [self initializeHealthKit:input callback:callback];
 }
 
+RCT_EXPORT_METHOD(clearSproutBackgroundTask:(NSDictionary *)input callback:(RCTResponseSenderBlock)callback)
+{
+    [self fitness_clearSproutBackgroundTask:input callback:callback];
+}
+
+RCT_EXPORT_METHOD(initSproutBackgroundTask:(NSDictionary *)input callback:(RCTResponseSenderBlock)callback)
+{
+    [self fitness_initializeSproutBackgroundTask:input callback:callback];
+}
+
 RCT_EXPORT_METHOD(initStepCountObserver:(NSDictionary *)input callback:(RCTResponseSenderBlock)callback)
 {
     [self fitness_initializeStepEventObserver:input callback:callback];
