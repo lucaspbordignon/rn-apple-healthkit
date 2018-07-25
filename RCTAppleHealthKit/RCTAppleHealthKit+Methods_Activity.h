075a70379cd19e8916b547047a5772e97aaff617
Steffen Oettich <soettich@bluewin.ch>
2018-07-26 00:06:27 +0200

    Add queries for daily samples of active and basal energy.

--- RCTAppleHealthKit/RCTAppleHealthKit+Methods_Activity.h
+++ RCTAppleHealthKit/RCTAppleHealthKit+Methods_Activity.h
@@ -10,6 +10,8 @@
 @interface RCTAppleHealthKit (Methods_Activity)
 
 - (void)activity_getActiveEnergyBurned:(NSDictionary *)input callback:(RCTResponseSenderBlock)callback;
+- (void)activity_getActiveEnergyDailySamples:(NSDictionary *)input callback:(RCTResponseSenderBlock)callback;
 - (void)activity_getBasalEnergyBurned:(NSDictionary *)input callback:(RCTResponseSenderBlock)callback;
+- (void)activity_getBasalEnergyDailySamples:(NSDictionary *)input callback:(RCTResponseSenderBlock)callback;
 
 @end
