d999314467846caded208d405ce231a22e69aa7b
Jesús Miguel Cruz Cana <jesus.cruz@mimacom.com>
2019-07-23 10:14:24 +0200

    Added reproductive health permissions.

--- RCTAppleHealthKit/RCTAppleHealthKit+TypesAndPermissions.m
+++ RCTAppleHealthKit/RCTAppleHealthKit+TypesAndPermissions.m
@@ -53,6 +53,12 @@
         @"MindfulSession" : [HKObjectType categoryTypeForIdentifier:HKCategoryTypeIdentifierMindfulSession],
         //workouts
         @"Workout" : [HKObjectType workoutType],
+        // Reproductive Health
+        @"BasalBodyTemperature" : [HKObjectType categoryTypeForIdentifier:HKQuantityTypeIdentifierBasalBodyTemperature],
+        @"MenstruationFlow" : [HKObjectType categoryTypeForIdentifier:HKCategoryTypeIdentifierMenstrualFlow],
+        @"OvulationTestResult" : [HKObjectType categoryTypeForIdentifier:HKCategoryTypeIdentifierOvulationTestResult],
+        @"SexualActivity" : [HKObjectType categoryTypeForIdentifier:HKCategoryTypeIdentifierSexualActivity],
+        
     };
     return readPerms;
 }
