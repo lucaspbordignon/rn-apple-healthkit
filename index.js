76f146b81e6901a5435684098c80bed5e39ca58c
Sandra Gomez <sandra.gomez@mimacom.com>
2019-07-23 13:05:04 +0200

    Created method to retrieve any nutrition sample

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-'use strict'
+'use strict';
 
 let { AppleHealthKit } = require('react-native').NativeModules;
 
-import { Permissions } from './Constants/Permissions'
-import { Units } from './Constants/Units'
+import { Permissions } from './Constants/Permissions';
+import { Units } from './Constants/Units';
+import { NutritionIntakes } from './Constants/NutritionIntakes';
 
 let HealthKit = Object.assign({}, AppleHealthKit, {
-	Constants: {
-		Permissions: Permissions,
-		Units: Units,
-	}
+  Constants: {
+    NutritionIntakes,
+    Permissions: Permissions,
+    Units: Units
+  }
 });
 
-export default HealthKit
+export default HealthKit;
 module.exports = HealthKit;
