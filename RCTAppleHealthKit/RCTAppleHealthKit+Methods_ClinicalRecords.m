1a17daf5b8a592364f5bcc824d642a6aa82e6ee9
Rohan <rohan@doc.ai>
2020-08-03 22:21:14 -0700

    Merged from original repo.

--- RCTAppleHealthKit/RCTAppleHealthKit+Methods_ClinicalRecords.m
+++ RCTAppleHealthKit/RCTAppleHealthKit+Methods_ClinicalRecords.m
@@ -326,36 +326,4 @@
     }
 }
 
-
-- (void)clinical_getMedicationRecord:(NSDictionary *)input callback:(RCTResponseSenderBlock)callback
-{
-    HKQuantityType *medicationRecordType = [HKQuantityType quantityTypeForIdentifier:HKClinicalTypeIdentifierMedicationRecord];
-
-    HKClinicalRecord
-    
-    let medicationsType = HKObjectType.clinicalType(forIdentifier: .medicationRecord) else {
-        callback(@[RCTMakeError(@"Unable to getting medicationRecord", nil, nil)]);
-    }
-
-    
-    NSPredicate *predicate = [RCTAppleHealthKit predicateForSamplesBetweenDates:startDate endDate:endDate];
-    NSUInteger limit = [RCTAppleHealthKit uintFromOptions:input key:@"limit" withDefault:HKObjectQueryNoLimit];
-    
-    
-    [self fetchSleepCategorySamplesForPredicate:predicate
-                                            limit:limit
-                                        completion:^(NSArray *results, NSError *error) {
-                                            if(results){
-                                                callback(@[[NSNull null], results]);
-                                                return;
-                                            } else {
-                                                NSLog(@"error getting sleep samples: %@", error);
-                                                callback(@[RCTMakeError(@"error getting sleep samples", nil, nil)]);
-                                                return;
-                                            }
-                                        }];
-    
-}
-
-
 @end
