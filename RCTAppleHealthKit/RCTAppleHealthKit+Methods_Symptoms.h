b269e070a351287ed7cc72be60a30c674c63dbb6
Bart Widlarz <bartlomiejwidlarz@gmail.com>
2020-09-19 08:17:01 +0200

    Starting bridging symptoms - bloating

---
+++ RCTAppleHealthKit/RCTAppleHealthKit+Methods_Symptoms.h
@@ -0,0 +1,13 @@
+//
+//  RCTAppleHealthKit+Methods_Symptoms.h
+//  RCTAppleHealthKit
+//
+//  Created by Bartek Widlarz on 18/09/2020.
+//  Copyright © 2020 Greg Wilson. All rights reserved.
+//
+
+#ifndef RCTAppleHealthKit_Methods_Symptoms_h
+#define RCTAppleHealthKit_Methods_Symptoms_h
+
+
+#endif /* RCTAppleHealthKit_Methods_Symptoms_h */
---
+++ RCTAppleHealthKit/RCTAppleHealthKit+Methods_Symptoms.m
@@ -0,0 +1,9 @@
+//
+//  RCTAppleHealthKit+Methods_Symptoms.m
+//  RCTAppleHealthKit
+//
+//  Created by Bartek Widlarz on 18/09/2020.
+//  Copyright © 2020 Greg Wilson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
